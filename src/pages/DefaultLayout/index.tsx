0ae0aabd65280a51b2558942db224faf606b60c1
AugustoMarcelo <mrclgst10@gmail.com>
2020-07-30 17:39:28 -0300

    Eslint fixes

--- src/pages/DefaultLayout/index.tsx
+++ src/pages/DefaultLayout/index.tsx
@@ -4,16 +4,14 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { Content } from './styles';
 
-const DefaultLayout: React.FC = ({ children }) => {
-  return (
-    <>
-      <Header />
-        <Content>
-          {children}
-        </Content>
-      <Footer />
-    </>
-  );
-}
+const DefaultLayout: React.FC = ({ children }) => (
+  <>
+    <Header />
+    <Content>
+      {children}
+    </Content>
+    <Footer />
+  </>
+);
 
 export default DefaultLayout;
