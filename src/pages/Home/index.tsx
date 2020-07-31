a6935ab2d7c5212b8b5a38a1b2eb7de6421935cc
AugustoMarcelo <mrclgst10@gmail.com>
2020-07-31 20:04:25 -0300

    Added modal to show movie and banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,18 +3,30 @@ import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 import Fab from '../../components/Fab';
+import Loading from '../../components/Loading';
 import Carousel from '../../components/Carousel';
+import Banner from '../../components/Banner';
 import DefaultLayout from '../DefaultLayout';
+import ShowMovieModal from './ShowMovieModal';
 
 export interface IMovie {
   id: string;
   title: string;
   phase: number;
+  saga?: string;
+  trailer_url?: string;
+  overview?: string;
   cover_url: string;
+  box_office?: number;
+  directed_by?: string;
+  release_date?: Date;
+  post_credit_scenes?: string;
 }
 
 const Home: React.FC = () => {
-  const [, setMovies] = useState<IMovie[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieToShow, setMovieToShow] = useState<IMovie>({} as IMovie);
   const [phaseOne, setPhaseOne] = useState<IMovie[]>([]);
   const [phaseTwo, setPhaseTwo] = useState<IMovie[]>([]);
   const [phaseThree, setPhaseThree] = useState<IMovie[]>([]);
@@ -24,7 +36,6 @@ const Home: React.FC = () => {
     async function loadMovies() {
       const { data } = await api.get<{ data: IMovie[], total: number }>('movies');
 
-      setMovies(data.data);
       setPhaseOne(data.data.filter((movie) => movie.phase === 1));
       setPhaseTwo(data.data.filter((movie) => movie.phase === 2));
       setPhaseThree(data.data.filter((movie) => movie.phase === 3));
@@ -35,21 +46,43 @@ const Home: React.FC = () => {
       if (registeredMovies) {
         setComingSoon((state) => [...state, ...JSON.parse(registeredMovies)]);
       }
+      setLoading(false);
     }
 
     loadMovies();
   }, []);
 
+  function toggleModal(): void {
+    setModalOpen(!modalOpen);
+  }
+
+  function showMovieInfo(movie: IMovie): void {
+    setMovieToShow(movie);
+    toggleModal();
+  }
+
   return (
     <DefaultLayout>
+      {loading ? (
+        <Loading sizeIcon={24} colorIcon="#fff" message="Loading Movies..." />
+      ) : (
+        <>
+          <Banner />
 
-      <Carousel title="Phase One" movies={phaseOne} />
-      <Carousel title="Phase Two" movies={phaseTwo} />
-      <Carousel title="Phase Three" movies={phaseThree} />
+          <Carousel title="Phase One" movies={phaseOne} showMovieInfo={showMovieInfo} />
+          <Carousel title="Phase Two" movies={phaseTwo} showMovieInfo={showMovieInfo} />
+          <Carousel title="Phase Three" movies={phaseThree} showMovieInfo={showMovieInfo} />
 
-      <Carousel title="Coming Soon" movies={comingSoon} />
+          <Carousel title="Coming Soon" movies={comingSoon} showMovieInfo={showMovieInfo} />
+        </>
+      )}
 
       <Fab to="/create" />
+      <ShowMovieModal
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        movieInfo={movieToShow}
+      />
     </DefaultLayout>
   );
 };
